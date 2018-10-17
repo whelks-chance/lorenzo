b61b70c5e8f0b038871ff206d40f125449ee2cf2
whelks-chance <iharvey111@gmail.com>
2018-10-17 17:01:06 +0100

    classes

--- extract_bid_filename_data.py
+++ extract_bid_filename_data.py
@@ -8,21 +8,31 @@ def extract_bids_filename_data(filename):
     data = {
         '_filename': str(filename)
     }
-    if filename.count('.') > 1:
-        # Handle the case where a file as an extra extension, e.g. .bak,  by removing it
-        reverse_index_of_last_dot = len(filename) - filename.rfind('.')
-        filename = filename[:-reverse_index_of_last_dot]
+    # if filename.count('.') > 1:
+    #     # Handle the case where a file as an extra extension, e.g. .bak,  by removing it
+    #     reverse_index_of_last_dot = len(filename) - filename.rfind('.')
+    #     filename = filename[:-reverse_index_of_last_dot]
+
+    filename_no_ext = filename
     if '.' in filename:
-        base_filename, extension = filename.split('.')
+        filename_arr = filename.split('.')
+
+        base_filename, extension = filename_arr[:2]
         data['_extension'] = str(extension)
-        key_value_pairs = base_filename.split('_')
-        for pair in key_value_pairs:
-            if '-' in pair:
-                key, value = pair.split('-')
-                data[key] = value
-            else:
-                # If there's no - in the pair this might be a simple filename, e.g. ClassFile.cls
-                if len(key_value_pairs) > 1:
-                    # There is only a postfix when the filename contains at least one key/value pair
-                    data['_postfix'] = str(pair)
+
+        long_extension = '.'.join(filename_arr[1:])
+        data['_long_extension'] = long_extension
+
+        filename_no_ext = filename_arr[0]
+
+    key_value_pairs = filename_no_ext.split('_')
+    for pair in key_value_pairs:
+        if '-' in pair:
+            key, value = pair.split('-')
+            data[key] = value
+        else:
+            # If there's no - in the pair this might be a simple filename, e.g. ClassFile.cls
+            if len(key_value_pairs) > 1:
+                # There is only a postfix when the filename contains at least one key/value pair
+                data['_postfix'] = str(pair)
     return data
--- record_dir.py
+++ record_dir.py
@@ -2,6 +2,8 @@ import os
 import json
 import pprint
 import re
+from abc import ABCMeta, abstractmethod
+
 import requests
 import sys
 
@@ -11,12 +13,14 @@ from extract_bid_filename_data import extract_bids_filename_data
 
 class CKANbidsImport:
     def __init__(self):
+        self.current_bids_dataset = BidsDataset()
         self.ds_dirs = []
         self.subject_files = []
         self.errors = []
         self.all_exts = []
         self.all_exts_size = []
         self.file_data = {}
+        self.dataset_info = {}
 
     def print_all(self, dir_struc):
 
@@ -129,21 +133,21 @@ class CKANbidsImport:
                 response_obj2 = response2.json()
                 dataset_name = response_obj2['result']['name']
 
-        self.add_resource(
-            'Source - zip',
-            dataset_name,
-            '/home/ianh/cubric/lorenzo/sub-meguk0354/meg/sub-meguk0354_task-resteyesopen_meg.json.zip',
-            '',
-            "application/zip"
-        )
-
-        self.add_resource(
-            'Source - json',
-            dataset_name,
-            '/home/ianh/cubric/lorenzo/sub-meguk0354/meg/sub-meguk0354_task-resteyesopen_meg.json',
-            '',
-            "application/json"
-        )
+                # self.add_resource(
+                #     'Source - zip',
+                #     dataset_name,
+                #     '/home/ianh/cubric/lorenzo/sub-meguk0354/meg/sub-meguk0354_task-resteyesopen_meg.json.zip',
+                #     '',
+                #     "application/zip"
+                # )
+                #
+                # self.add_resource(
+                #     'Source - json',
+                #     dataset_name,
+                #     '/home/ianh/cubric/lorenzo/sub-meguk0354/meg/sub-meguk0354_task-resteyesopen_meg.json',
+                #     '',
+                #     "application/json"
+                # )
 
     def add_resource(self, resource_name, dataset_name, filepath, url, mimetype):
         data = {
@@ -153,7 +157,7 @@ class CKANbidsImport:
             # "format": "text/html"
             "mimetype": mimetype
         }
-        print('\n\n', data, '\n\n')
+        print('\n\nresource_create data', data, '\n\n')
 
         response = requests.post(
             settings.ckan_url + '/api/action/resource_create',
@@ -170,8 +174,26 @@ class CKANbidsImport:
         print(response.text)
         response_obj = response.json()
 
-    def print_file(self, path):
+        print('\n\n Adding resource_view_create')
+        data = {
+            "view_type": 'filetree_view',
+            "title": 'File Tree',
+            "resource_id": response_obj['result']['id']
+        }
+        response = requests.post(
+            settings.ckan_url + '/api/action/resource_view_create',
+            data=json.dumps(data),
+            headers={
+                "X-CKAN-API-Key": settings.ckan_api_key,
+                'content-type': 'application/json'
+            }
+        )
+        print(response.status_code)
+        print(response.reason)
+        print(response.text)
+        response_obj = response.json()
 
+    def print_file(self, path):
         with open(path,  'r') as file_handle:
             print(file_handle.read())
 
@@ -213,9 +235,31 @@ class CKANbidsImport:
             self.save_dataset(data)
         return data
 
+    # def is_gla_format(self, filename):
+    #     basename = os.path.basename(filename)
+    #     parent_dir = os.path.basename(os.path.dirname(filename))
+    #     return os.path.isdir(filename) and basename.endswith('_meg') and parent_dir == 'meg'
+    #
+    # def is_fif_dataset(self, filename):
+    #     basename = os.path.basename(filename)
+    #     parent_dir = os.path.basename(os.path.dirname(filename))
+    #     print(basename, '-----', parent_dir)
+    #     return basename.startswith('sub-') and basename.endswith('.fif') and parent_dir == 'meg'
+    #
+    # def is_meg_dataset(self, filename):
+    #     basename = os.path.basename(filename)
+    #     parent_dir = os.path.basename(os.path.dirname(filename))
+    #     return os.path.isdir(filename) and \
+    #            basename.startswith('sub') and \
+    #            basename.endswith('.ds') and \
+    #            parent_dir == 'meg'
+
     # Original crawling code from https://stackoverflow.com/a/25226267/2943238 Emanuele Paolini
     def path_to_dict(self, path):
-        d = {'name': os.path.basename(path)}
+        d = {
+            'name': os.path.basename(path),
+            'full_path': path
+        }
 
         if not os.access(path, os.R_OK):
             print('Cannot access ', path)
@@ -225,14 +269,34 @@ class CKANbidsImport:
         if d['name'].startswith('.') or d['name'].startswith('__'):
             return d
 
+        self.current_bids_dataset.record_if_dataset(path)
+        # # print('\nChecking', path, self.is_meg_dataset(path))
+        # if self.is_meg_dataset(path):
+        #     print('Is MEG')
+        #     self.current_bids_dataset.add_meg_dataset(path)
+        #     # print('\n\nDS folder', d, '\n\n')
+        #     self.ds_dirs.append(d)
+        #
+        # if self.is_fif_dataset(path):
+        #     print('Is fif')
+        #     self.current_bids_dataset.add_fif_dataset(path)
+        #     # print('\n\nDS folder', d, '\n\n')
+        #     self.ds_dirs.append(d)
+        #
+        # if self.is_gla_format(path):
+        #     print('Is gla')
+        #     self.current_bids_dataset.add_gla_dataset(path)
+        #     # print('\n\nDS folder', d, '\n\n')
+        #     self.ds_dirs.append(d)
+
         if os.path.isdir(path):
             d['type'] = "directory"
 
-            d['children'] = [self.path_to_dict(os.path.join(path,x)) for x in os.listdir(path)]
-
-            if d['name'].startswith('sub') and d['name'].endswith('.ds'):
-                print('\n\nDS folder', d, '\n\n')
-                self.ds_dirs.append(d)
+            d['children'] = [
+                self.path_to_dict(
+                    os.path.join(path, x)
+                ) for x in os.listdir(path)
+            ]
 
         else:
             d['type'] = "file"
@@ -245,37 +309,263 @@ class CKANbidsImport:
             })
 
             if d['name'].startswith('sub'):
-                d['full_path'] = path
                 self.subject_files.append(d)
 
         filename_metadata = extract_bids_filename_data(d['name'])
         self.file_data[path] = filename_metadata
         d['filename_metadata'] = filename_metadata
 
-            # if 'sub' in d['name'] and 'task' in d['name'] and d['ext'].endswith('json'):
-            #     self.json_to_keyvalue(path)
+        for k in filename_metadata.keys():
+            if not k.startswith('_'):
+                self.dataset_info[k] = filename_metadata[k]
+
+                # if 'sub' in d['name'] and 'task' in d['name'] and d['ext'].endswith('json'):
+                #     self.json_to_keyvalue(path)
 
         return d
+
+    def get_dataset_info(self, json_blob):
+        dataset_name = json_blob['filename_metadata']['sub']
+
+        data_dict = {
+            'dataset_name': dataset_name
+        }
+
+        field_maps = {
+            'sub': 'Subject',
+            'task': 'Task',
+            'meg': 'MEG Extension',
+            'ses': 'Session',
+            'run': 'Run',
+            'proc': 'Processed'
+        }
+
+        for k in json_blob['dataset_info'].keys():
+            if k in field_maps:
+                data_dict[field_maps[k]] = json_blob['dataset_info'][k]
+
+        return data_dict
+
+    def parse_metadata_json(self, path):
+        files = list(os.listdir(path))
+        for f in files[:2]:
+            filepath = os.path.join(path, f)
+            # print(filepath)
+
+            if '.ds.json' in f:
+                with open(filepath, 'r') as fp1:
+                    json_blob = json.load(fp1)
+
+                    self.add_resource(
+                        json_blob['name'],
+                        json_blob['filename_metadata']['sub'],
+                        filepath=filepath, url=None,
+                        mimetype='application/json'
+                    )
+            elif '-subjects.json':
+                pass
+            else:
+                with open(filepath, 'r') as fp2:
+                    json_blob = json.load(fp2)
+
+                    data_dict = self.get_dataset_info(json_blob)
+
+                    self.save_dataset(data_dict)
+
+
+class BidsResource(metaclass=ABCMeta):
+    MEG = 1
+    FIF = 2
+    GLA = 3
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.type = type
+
+    def basename(self):
+        return os.path.basename(self.dataset).split('_meg')[0]
+
+    def get_meg_ext(self):
+        idx = os.path.basename(self.dataset).rfind('_meg')
+        return os.path.basename(self.dataset)[idx:]
+
+    def get_subject(self):
+        return self.basename().split('_')[0].split('-')[1]
+
+    def get_task(self):
+        bits = self.basename().split('_')
+        for bit in bits:
+            if 'task' in bit:
+                return bit.split('-')[1]
+
+    def get_session(self):
+        bits = self.basename().split('_')
+        for bit in bits:
+            if 'ses' in bit:
+                return bit.split('-')[1]
+
+    def get_aquisition(self):
+        bits = self.basename().split('_')
+        for bit in bits:
+            if 'acq' in bit:
+                return bit.split('-')[1]
+
+    def get_processed(self):
+        bits = self.basename().split('_')
+        for bit in bits:
+            if 'proc' in bit:
+                return bit.split('-')[1]
+
+    def get_run(self):
+        bits = self.basename().split('_')
+        for bit in bits:
+            if 'run' in bit:
+                return bit.split('-')[1]
+
+    @abstractmethod
+    def get_resource_description(self):
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def matches_pattern(filename):
+        raise NotImplementedError
+
+
+class CDFBidsResource(BidsResource):
+
+    def get_resource_description(self):
+        return '{}\nSubject: {}\n' \
+               'Meg ext: {}'.format(
+            self.dataset,
+            self.get_subject(),
+            self.get_meg_ext()
+        )
+
+    @staticmethod
+    def matches_pattern(filename):
+        basename = os.path.basename(filename)
+        parent_dir = os.path.basename(os.path.dirname(filename))
+        return os.path.isdir(filename) and \
+               basename.startswith('sub') and \
+               '_task-' in basename and \
+               basename.endswith('_meg.ds') and \
+               parent_dir == 'meg'
+
+
+class FIFBidsResource(BidsResource):
+    def get_resource_description(self):
+        return '{}\nSubject: {}\n'.format(self.dataset, self.get_subject())
+
+    @staticmethod
+    def matches_pattern(filename):
+        basename = os.path.basename(filename)
+        parent_dir = os.path.basename(os.path.dirname(filename))
+        print(basename, '-----', parent_dir)
+        return basename.startswith('sub-') and \
+               '_task-' in basename and \
+               basename.endswith('_meg.fif') and \
+               parent_dir == 'meg'
+
+
+class GLABidsResource(BidsResource):
+    def get_resource_description(self):
+        return '{}\nSubject: {}\n'.format(self.dataset, self.get_subject())
+
+    @staticmethod
+    def matches_pattern(filename):
+        basename = os.path.basename(filename)
+        parent_dir = os.path.basename(os.path.dirname(filename))
+        return os.path.isdir(filename) and \
+               basename.startswith('sub-') and \
+               '_task-' in basename and \
+               basename.endswith('_meg') and \
+               parent_dir == 'meg'
+
+
+class BidsDataset:
+
+    def __init__(self):
+
+        self.dataset_resources = []
+        self.dataset_info = {}
+        self.root_dir = ''
+        self.root_path = ''
+
+    def known_dataset_types(self):
+        return [CDFBidsResource, FIFBidsResource, GLABidsResource]
+
+    def get_resources(self):
+        return self.dataset_resources
+
+    def __repr__(self):
+        # return str(self.get_resources())
+        return str(self.__dict__)
+
+    def description(self):
+        description_text = 'Bids Dataset\nHas {} MEG dataset(s).\n'.format(
+            len(self.dataset_resources)
+        )
+        description_text += 'Root folder is {}\n'.format(self.root_dir)
+        description_text += 'Root path is {}\n'.format(self.root_path)
+        description_text += 'Resources: {}'.format(self.get_resource_descriptions())
+
+        return description_text
+
+    def get_resource_descriptions(self):
+        description = ''
+        for r in self.get_resources():
+            description += str(r.get_resource_description()) + '\n'
+        return description
+
+    def record_if_dataset(self, path):
+        for t in self.known_dataset_types():
+            if t.matches_pattern(path):
+                print('Match {}, {}'.format(t, path))
+                self.dataset_resources.append(t(path))
 
 
 if __name__ == '__main__':
 
+    bids_datasets = []
+
     cbi = CKANbidsImport()
-    path = '/home/ianh/cubric/lorenzo/sub-meguk0354'
+    # sub-meguk0354
+    # path = '/home/ianh/cubric/lorenzo/'
+
+    path = '/home/ianh/PycharmProjects/lorenzo/data'
 
     if len(sys.argv) > 1:
         path = sys.argv[1]
 
     try:
+        # making output directory
         os.mkdir('json')
     except:
         pass
 
     for ds in os.listdir(path):
+        # print(ds)
+        # loop through sub dirs, look for sub-
         dir_path = os.path.join(path, ds)
         if ds.startswith('sub') and os.path.isdir(dir_path):
 
+            bids_dataset = BidsDataset()
+
+            cbi.current_bids_dataset = bids_dataset
+
+            # The main workhorse of the import class
+            # Takes a folder and traverses the whole structure,
+            # looking for meg datasets and sidecars files
+            # to add as resources to a bids dataset
+            # returns a complex dict of the folder structure
+            # internally updates the current_bids_dataset with what it discovered along the way
             dir_struc = cbi.path_to_dict(dir_path)
+            dir_struc['dataset_info'] = cbi.dataset_info
+
+            bids_dataset.dataset_info = cbi.dataset_info
+            bids_dataset.root_dir = ds
+            bids_dataset.root_path = dir_path
 
             # cbi.print_all(dir_struc)
             output_file = os.path.join('./json', '{}.json'.format(ds))
@@ -296,6 +586,9 @@ if __name__ == '__main__':
             # Reset things for the next dir
             cbi.ds_dirs = []
             cbi.subject_files = []
+            cbi.dataset_info = {}
+
+            bids_datasets.append(bids_dataset)
 
     with open('errors.log', 'w') as err_file:
         err_file.write(
@@ -303,3 +596,11 @@ if __name__ == '__main__':
                 sorted(cbi.errors),
                 indent=4)
         )
+
+    for bd in bids_datasets:
+        assert isinstance(bd, BidsDataset)
+        print('\n***\n')
+        print(bd.description())
+
+        # cbi.parse_live_json('./json_live')
+
